faf6439ddd516b4fe0ac5ca72de3b24023c77d4e
hannah <hannahnajwa@gmail.com>
2021-08-14 16:32:27 +0800

    testing services with dependencies (no TestBed yet)

--- src/app/demo/demo.spec.ts
+++ src/app/demo/demo.spec.ts
@@ -1,4 +1,8 @@
-import {ValueService} from './demo';
+import {MasterService, ValueService} from './demo';
+
+export class FakeValueService extends ValueService {
+  value = 'faked service value';
+}
 
 describe('ValueService', () => {
   let service: ValueService;
@@ -22,5 +26,35 @@ describe('ValueService', () => {
       expect(value).toBe('promise value');
       done();
     });
+  });
+});
+
+describe('MasterService without Angular testing support', () => {
+  let masterService: MasterService;
+
+  it('#getValue should return real value from the real service', () => {
+    masterService = new MasterService(new ValueService());
+    expect(masterService.getValue()).toBe('real value');
+  });
+
+  it('#getValue should return faked value from a fakeService', () => {
+    masterService = new MasterService(new FakeValueService());
+    expect(masterService.getValue()).toBe('faked service value');
+  });
+
+  it('#getValue should return stubbed value from a spy', () => {
+    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+
+    const stubValue = 'stub value';
+    valueServiceSpy.getValue.and.returnValue(stubValue);
+
+    masterService = new MasterService(valueServiceSpy);
+
+    expect(masterService.getValue())
+      .toBe(stubValue, 'service returned stub value');
+    expect(valueServiceSpy.getValue.calls.count())
+      .toBe(1, 'spy method was called once');
+    expect(valueServiceSpy.getValue.calls.mostRecent().returnValue)
+      .toBe(stubValue);
   });
 });
